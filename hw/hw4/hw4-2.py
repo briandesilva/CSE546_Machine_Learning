5c21f917719fd806315b995f160113aec30f0cfa
Brian de Silva <bdesilva@uw.edu>
2016-12-07 11:14:38 -0800

    Added more code for problem 2

--- hw/hw4/hw4-2.py
+++ hw/hw4/hw4-2.py
@@ -32,7 +32,7 @@ numClasses = 10
 # ----------------------------------------------------------------------------------------
 
 
-# Neural network class with tanh activation functions
+# Neural network class using tanh activation function at hidden layer
 class NN:
 	# Constructor where weights are pre-specified
 	def __init__(self,W_i,W_h):
@@ -71,25 +71,33 @@ class NN:
 		# 	probs[j,:] = np.exp(self.ys[j-1,:] + denom)
 		# return probs
 
-	# Computes the gradient of the square loss wrt the weights in the NN
-	# Y should be a matrix with each column a length 10 one-hot vector of class labels
-	# Y should have already been sampled
-	def backProp(self,X,Y,sampleIndices):
-		dW_h = -(Y - self.oa[:,sampleIndices])       				# note: activation function is identity
-		dW_i = self.W_h.T.dot(dW_h) * (1 - (self.hz[:,sampleIndices]) ** 2)		# Sech^2(x) = 1 - Tanh^2(x)
-		return (dW_h.dot(self.ha[:,sampleIndices].T),dW_i.dot(X.T))
+	# # Computes the gradient of the square loss wrt the weights in the NN
+	# # Y should be a matrix with each column a length 10 one-hot vector of class labels
+	# # Y should have already been sampled
+	# def backPropPartial(self,X,Y,sampleIndices):
+	# 	dW_h = -(Y - self.oa[:,sampleIndices])       				# note: activation function is identity
+	# 	dW_i = self.W_h.T.dot(dW_h) * (1 - (self.hz[:,sampleIndices]) ** 2)		# Sech^2(x) = 1 - Tanh^2(x)
+	# 	return (dW_h.dot(self.ha[:,sampleIndices].T),dW_i.dot(X.T))
 
 
 	# Computes the gradient of the square loss wrt the weights in the NN
 	# Y should be a matrix with each column a length 10 one-hot vector of class labels
-	# Y should have already been sampled
 	# steps := (stepsize for W_i, stepsize for W_h)
-	def backPropFull(self,X,Y,steps):
-		dW_h = -(Y - self.oa)       				# note: activation function is identity
-		dW_i = self.W_h.T.dot(dW_h) * (1 - (self.hz) ** 2)		# Sech^2(x) = 1 - Tanh^2(x)
-		self.W_h -= steps[1] * dW_h
-		self.W_i -= steps[0] * dW_i
+	def backProp(self,X,Y,steps):
+		N = X.shape[1]
+		dW_h = -(Y - self.oa) / N       				# note: activation function is identity
+		dW_i = self.W_h.T.dot(dW_h) * (1 - (self.hz) ** 2) / N		# Sech^2(x) = 1 - Tanh^2(x)
+		self.W_h -= steps[1] * dW_h.dot(self.ha.T)
+		self.W_i -= steps[0] * dW_i.dot(X.T)
 		# return (dW_h.dot(self.ha.T),dW_i.dot(X.T))
+
+	# A function that returns the gradients of the two weight matrices
+	def getGrads(self,X,Y):
+		self.forwardPass(X)
+		N = X.shape[1]
+		dW_h = -(Y - self.oa) / N       				# note: activation function is identity
+		dW_i = self.W_h.T.dot(dW_h) * (1 - (self.hz) ** 2) / N		# Sech^2(x) = 1 - Tanh^2(x)
+		return (dW_i.dot(X.T), dW_h.dot(self.ha.T))
 
 
 	# Store the weights in files in case we need to re-use them
@@ -99,12 +107,29 @@ class NN:
 
 
 	# Compute the mean square loss
+	# Y should be a matrix with each column a length 10 one-hot vector of class labels
 	def squareLoss(self,X,Y):
 		self.forwardPass(X)
-		
+		return np.mean(np.sum((Y - self.oa)**2,0))
 
+	# Compute the 0/1 loss
+	# Y should be a vector of class labels
+	def z1Loss(self,X,Y):
+		self.forwardPass(X)
+		return (1.0 * np.count_nonzero(np.argmax(self.oa,0) - Y)) / Y.shape[0]
 
+	# Compute mean square loss and 0/1 loss
+	# Y should be a matrix with each column a length 10 one-hot vector of class labels
+	def getLoss(self,X,Y):
+		self.forwardPass(X)
 
+		# Compute square loss
+		sql = np.mean(np.sum((Y - self.oa)**2,0))
+
+		# Compute 0/1 loss
+		z1l = 1. * np.count_nonzero(np.argmax(Y,0) != np.argmax(self.oa,0)) / Y.shape[1]
+
+		return (sql,z1l)
 
 
 
@@ -126,48 +151,116 @@ testProj = test_img.dot(V[:50,:].T)
 
 # Create binary labels
 trainBinLabels = np.zeros((10,len(train_label)))
+testBinLabels = np.zeros((10,len(test_label)))
 labVals = range(10)
 for k in range(len(train_label)):
-	trainBinLabels[:,k] = train_label[k] == labVals
+	trainBinLabels[:,k] = (train_label[k] == labVals)
+	testBinLabels[:,k] = (test_label[k] == labVals)
 
 
 # Set parameters
 # --------------------------------------------------------------------------
-nClasses = 10		# Number of classes
-reg = 0.			# Regularization parameter
-batchSize = 10		# Batch size for SGD
-numEpochs = 10		# Number of epochs to go through before terminating
-# --------------------------------------------------------------------------
+nClasses = 10			# Number of classes
+reg = 0.				# Regularization parameter
+batchSize = 10			# Batch size for SGD
+numEpochs = 10			# Number of epochs to go through before terminating
+nnSize = (50,500,10)	# Number of nodes in each layer of NN (input, hidden, output)
+std = (np.sqrt(np.mean(trainProj**2)),np.sqrt(50))	# Initial weight standard deviations
+ckptStr = "init_test2.1"	# String used in checkpointing filename
 
 print "Time elapsed during setup: %f" %(time.time() - t1)
+# --------------------------------------------------------------------------
 
 
+# 
+# Train the network
+# 
 
 # Initialize the neural net
-nn = NN((50,500,10))
+nn = NN(nnSize,std)
 nn.forwardPass(trainProj.T)
 
-# TODO: figure out scaling for initial weights
+# Rescale the weights so that E(Yhat) ~ E(Y) / 10
+# Note: E(Y) = 1/10
+nn.W_h /= (100*np.mean(nn.oa))
+nn.W_i /= (100*np.mean(nn.oa))
+
+# Create vectors in which to store the loss
+trainLoss = np.zeros((2,2 * numEpochs + 1))		# First row is sq loss, 2nd is 0/1
+testLoss = np.zeros((2,2 * numEpochs + 1))		# First row is sq loss, 2nd is 0/1
 
 
-# Gradient descent step
-step = 1.e-6
-
-# For loop:
-
-# Get new permutation of the inputs
+# SGD
+step = 1.e-4				# Learning rate
 N = train_img.shape[0]
-sampleIndices = np.random.permutation(N)
 
-for subIt in range(0, N / batchSize):
+# Loop over epochs
+for it in range(numEpochs):
+	t2 = time.time()
+
+	# Get new sampling order for this epoch
+	sampleIndices = np.random.permutation(N)
+
+	# Compute losses
+	trainLoss[:,2*it] = nn.getLoss(trainProj.T,trainBinLabels)
+	testLoss[:,2*it] = nn.getLoss(testProj.T,testBinLabels)
+
+	# Print losses
+	print "Square loss after %d epochs: %f"%(it,trainLoss[0,2*it])
+	print "0/1 loss after %d epochs: %f"%(it,trainLoss[1,2*it])
+
+	# Take multiple gradient steps for half an epoch
+	for subIt in range((N / batchSize) / 2):
+		# Get indices for this iteration
+		currIndices = sampleIndices[subIt*batchSize:(subIt+1)*batchSize]
+
+		# Forward pass
+		nn.forwardPass(trainProj[currIndices,:].T)
+
+		# Update weights
+		nn.backProp(trainProj[currIndices,:].T,trainBinLabels[:,currIndices],(step,step))
+
+
+
+	# Compute and print losses after half an epoch
+	trainLoss[:,2*it+1] = nn.getLoss(trainProj.T,trainBinLabels)
+	testLoss[:,2*it+1] = nn.getLoss(testProj.T,testBinLabels)
+	print "Square loss after %d 1/2 epochs: %f"%(it,trainLoss[0,2*it+1])
+	print "0/1 loss after %d 1/2 epochs: %f"%(it,trainLoss[1,2*it+1])
+
+	# Check the norms of the full gradients
+	(gi,gh) = nn.getGrads(trainProj.T,trainBinLabels)
+	print "Orders of gradients W_i, W_h: %f, %f"%(np.sqrt(np.sum(gi**2)),np.sqrt(np.sum(gh**2)))
+
+	# Finish the epoch
+	for subIt in range((N / batchSize) / 2, N / batchSize):
+		# Get indices for this iteration
+		currIndices = sampleIndices[subIt*batchSize:(subIt+1)*batchSize]
+
+		# Forward pass
+		nn.forwardPass(trainProj[currIndices,:].T)
+
+		# Update weights
+		nn.backProp(trainProj[currIndices,:].T,trainBinLabels[:,currIndices],(step,step))
+
 	
-	# Forward pass
-	nn.forwardPass(trainProj[sampleIndices,:].T)
 
-	# Perform backpropagation and update weights with an SGD step
-	nn.backPropFull(trainProj[sampleIndices,:].T,trainBinLabels[:,sampleIndices])
-	# (dW_h,dW_i) = nn.backPropFull(trainProj[sampleIndices,:].T,trainBinLabels[:,sampleIndices])
 
-	# # Take gradient descent step (build this into backprop?)
-	# nn.W_h -= step * dW_h
-	# nn.W_i -= step * dW_h
+# # For loop:
+
+# # Get new permutation of the inputs
+# N = train_img.shape[0]
+# sampleIndices = np.random.permutation(N)
+
+# for subIt in range(0, N / batchSize):
+	
+# 	# Forward pass
+# 	nn.forwardPass(trainProj[sampleIndices,:].T)
+
+# 	# Perform backpropagation and update weights with an SGD step
+# 	nn.backPropFull(trainProj[sampleIndices,:].T,trainBinLabels[:,sampleIndices])
+# 	# (dW_h,dW_i) = nn.backPropFull(trainProj[sampleIndices,:].T,trainBinLabels[:,sampleIndices])
+
+# 	# # Take gradient descent step (build this into backprop?)
+# 	# nn.W_h -= step * dW_h
+# 	# nn.W_i -= step * dW_h
